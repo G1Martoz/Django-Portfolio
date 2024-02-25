d468b8329198ecc3d55a40405f044ff5cc06dc65
G1Martoz <g1martinozuna@gmail.com>
2024-02-25 20:14:54 -0300

    first commit

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 
 
-#! DATABASES = {
+# DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
@@ -70,9 +70,11 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    # Replace this value with your local database's connection string.
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600)
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
