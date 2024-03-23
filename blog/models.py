5223c65aa0727ee7d8d3f09c37242c28c11394bd
G1Martoz <g1martinozuna@gmail.com>
2024-03-22 21:29:45 -0300

    Templates

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,11 @@ import datetime
 
 
 class Post(models.Model):
-    # Corregí el typo en 'tittle' a 'title'
+    
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='blog/images')
-    # Corregí aquí también
+    
     date = models.DateField(default=datetime.date.today)
 
     def __str__(self):
